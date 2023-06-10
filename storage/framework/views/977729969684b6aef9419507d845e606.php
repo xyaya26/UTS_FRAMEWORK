bdbb547863b7903883cc8d2bae19c93bce25f199
xyaya26 <108610792+xyaya26@users.noreply.github.com>
2023-06-10 20:21:59 +0700

    Add files via upload

---
+++ storage/framework/views/977729969684b6aef9419507d845e606.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar Controller & View pada Laravel</title>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/sass/app.scss'); ?>
+</head>
+
+<body>
+    <div class="container text-center my-5">
+        <h1 class="mb-4">Belajar Controller & View pada Laravel</h1>
+        
+        <img src="<?php echo e(Vite::asset('resources/images/laravel.png')); ?>" alt="image" class="img-thumbnail">
+        <div class="col-md-2 offset-md-5 mt-4">
+            <div class="d-grid gap-2"><a href="<?php echo e(route('home')); ?>" class="btn btn-dark">Home</a></div>
+        </div>
+    </div>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+</body>
+
+</html>
+<?php /**PATH D:\FemWokPurp\PrakMod05-new\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d91ac24d630230abecd0ea078ec0084c.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar Laravel Routing</title>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/sass/app.scss'); ?>
+</head>
+
+<body>
+    <div class="container m-5">
+        <h1>Belajar Laravel Routing</h1>
+        <div class="list-group list-group-numbered mt-4">
+
+        </div>
+    </div>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+</body>
+
+</html>
+<?php /**PATH D:\FemWokPurp\PrakMod04\resources\views/routing.blade.php ENDPATH**/ ?>
